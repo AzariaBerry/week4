7db83aa0fa99ebddd51d6f1ed4f659b675d87d10
Azaria berry <azariaberry2019@outlook.com>
2020-07-14 16:57:45 -0400

    homework

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-console.log("hello");
-var name="Azaria"
-var count= 10
+//monday homework commented out for tuesday homework
+
+//console.log("hello");
+//var name="Azaria"
+//var count= 10
 /* i got Nan for this because name is not a number so it cant be multiplied */
-console.log(count * name)
+//console.log(count * name)
 /* i got 3 for this because true represents as 1 */
-console.log(2 + true);
+//console.log(2 + true);
+
+//tuesday homework
+//Did in code pen to get easy visual of my work
+var count= 2;
+console.log(count+5);
+console.log((count+5)*2);
+//I got 14 when i multiplied the result by 2
+//i think the answer will be a decimal because 3 cant evenly go into 14
+console.log(14%3);
+//no it put 2 instead of a decimal. i forgot it puts the remainder.
+
+var count= 734;
+if(count % 2 ==0){
+    console.log("even");
+}else if(count % 2 !=0){
+    console.log("odd");
+}
+
+
+var wannaPlaySpongebob = true;
+var wannaSleep = true;
+
+if(wannaPlaySpongebob && wannaSleep){
+    console.log("Play in dreams!");
+} else {
+    console.log("Do one of them");
+}
