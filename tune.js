6b4574bc0c184848f427248ed1656d635de8f8c2
Azaria berry <azariaberry2019@outlook.com>
2020-07-19 18:15:44 -0400

    made look better

--- tune.js
+++ tune.js
@@ -1,9 +1,11 @@
 var array = [];
 
 function songRequest() {
-
+//pushes what ever is typed for song request into the array
     array.push(document.getElementById("userRequest").value + "<br>");
+//displays whatever song is requested in the html
     var Requests = document.getElementById("userRequest").value;
+//the songs requested will also display in the array
     document.getElementById("song").innerHTML = array;
     console.log(array);
 }
